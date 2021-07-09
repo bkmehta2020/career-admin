30a9f6d5386c1e575791625f30d534778d49c823
[your <bkmehta@newageproducts.com>
2021-07-09 19:08:02 +0530

    chages

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e45651d96a493d8fc4e1ae334519b188'),
+    },
+  },
+});
